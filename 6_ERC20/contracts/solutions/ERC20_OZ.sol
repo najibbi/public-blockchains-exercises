3b4fe2aef4db7b67a9fea7c064772bf065312a38
najibbirouk <najibbirouk@hotmail.fr>
2023-04-18 15:28:29 +0200

    commit 1 zippielin

--- 6_ERC20/contracts/solutions/ERC20_OZ.sol
+++ 6_ERC20/contracts/solutions/ERC20_OZ.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.17;
 
 // Learn more about the ERC20 implementation 
 // on OpenZeppelin docs: https://docs.openzeppelin.com/contracts/4.x/erc20
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20_OZ is ERC20 {
     constructor() ERC20("MY_OWN_TOKEN", "MOT") {
